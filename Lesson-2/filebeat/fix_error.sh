2916939ba2f375880e888b0105207ecb17e871af
thangnv-sre <thang.nguyenvan@pixta.co.jp>
2022-10-05 13:40:47 +0700

    update nginx

---
+++ Lesson-2/filebeat/fix_error.sh
@@ -0,0 +1,2 @@
+sudo chown root filebeat.docker.yml
+sudo chmod go-w filebeat.docker.yml
---
+++ Lesson-2/scrip.sh
@@ -0,0 +1,4 @@
+############## Lab1, Cài đặt kibana
+cd ELK-tech-master/Lesson-2/kibana
+docker-compose up -d
+docker-compose logs -f
