132783cb9860253a917e96578085901082466772
Ubuntu <ubuntu@ip-172-31-32-174.ap-southeast-1.compute.internal>
2022-10-29 10:19:26 +0000

    Update lesson-07

--- Lesson-6/check_telegram.sh
+++ Lesson-6/check_telegram.sh
@@ -1,5 +1,5 @@
-TOKEN="5573884598:AAGMvBN4QGwMXNuC_jzwA2_UB8bL0QTXyRY" 
-ID="-898635285"
+TOKEN="5596884767:AAHwneD-u5wTKjJncM3NiX_pv69_gEpCGGU" 
+ID="-888913730"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
-curl -s -X POST $URL -d chat_id=$ID -d text="Hello World"
+curl -s -X POST $URL -d chat_id=$ID -d text="Hello thangnv"
---
+++ Lesson-7/text.sh
@@ -0,0 +1,7 @@
+# Install minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+
+
+kubectl get event  --field-selector involvedObject.name=elasticsearch-master-0
